95d0c1be9e202c221045a178e2dff1b2cfe85ce3
sophiebl <sophieboulaaouli@gmail.com>
2020-07-10 11:40:34 +0200

    Fetch Data stories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import  React, { useState, useEffect } from "react";
+import axios from "axios";
+// import Header from "./components/header/header.js";
+// import Main from "./components/main/main.js";
+import logo from './logo.gif';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const [posts, setPosts] = useState(null);
+
+const fetchDataNews = async () => {
+  const response = await axios.get(
+    'https://hacker-news.firebaseio.com/v0/topstories.json');
+    setPosts(response.data);
+  };
+
+
+return(
+  <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>
+          Hacker News
+      </h1>
+      <nav>
+          <ul>
+              <li onClick={fetchDataNews}>New</li>
+              <li onClick={fetchDataPast}>Pasts</li>
+              <li>Comments</li>
+              <li>Ask</li>
+              <li>Show</li>
+              <li>Jobs</li>
+          </ul>
+      </nav>
+      <div className="posts">
+      {posts &&
+        posts.map((post, index) => {
+
+          return (
+            <div className="post" key={index}>
+              <h3>Post {index + 1}</h3>
+            </div>
+          );
+        })}
     </div>
-  );
+  </header>
+)
 }
 
 export default App;
---
+++ src/components/main/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Main = () => (
+        <div>
+            MAIN
+        </div>
+);
+
+
+export default Main;
